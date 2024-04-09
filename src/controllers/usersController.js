07e43daeb41c537065e287c51ba6c452002e70dd
N4huel17 <jesusdsm17@gmail.com>
2024-04-09 20:31:16 -0300

    actualizar datos de perfil

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -84,7 +84,54 @@ module.exports={
                 if (!user) {
                     return res.status(404).send("Usuario no encontrado");
                 }
-                return res.render('users/perfil', { user });
+    
+                // Verificamos si el usuario ha solicitado cambiar la contraseña
+                const changePasswordRequested = req.query.change_password === 'true';
+                
+                return res.render('users/perfil', { user, changePasswordRequested });
+            })
+            .catch(error => {
+                console.error(error);
+                return res.status(500).send("Error interno del servidor");
+            });
+    },
+    actualizarPerfil: (req, res) => {
+        const userId = req.session.userLogin.id;
+        const { name, surname, email, password, confirmPassword } = req.body;
+    
+        if (password !== confirmPassword) {
+            return res.status(400).send("Las contraseñas no coinciden");
+        }
+    
+        // Encripta la contraseña antes de guardarla
+        let hashedPassword = null;
+        if (password.trim() !== '') {
+            hashedPassword = hashSync(password.trim(), 10);
+        }
+    
+        db.users.findByPk(userId)
+            .then(user => {
+                if (!user) {
+                    return res.status(404).send("Usuario no encontrado");
+                }
+    
+                // Actualiza los datos del usuario
+                user.name = name;
+                user.surname = surname;
+                user.email = email;
+                
+                // Actualiza la contraseña solo si se proporciona una nueva
+                if (hashedPassword) {
+                    user.password = hashedPassword;
+                }
+    
+                // Guarda los cambios en la base de datos
+                return user.save();
+            })
+            .then(updatedUser => {
+                // Redirige o envía una respuesta de éxito con un mensaje
+                const changePasswordRequested = false; // Asegúrate de que changePasswordRequested sea false aquí
+                return res.render('users/perfil', { user: updatedUser, successMessage: "Datos guardados exitosamente", changePasswordRequested });
             })
             .catch(error => {
                 console.error(error);
